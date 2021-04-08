883caf86a9e02349f93e4b1d6edef8ab4e80dde5
b7tarts <sandro.tartoni@swisslog.com>
2021-04-08 15:36:58 +0200

    -fixed issue where you are unable to change value in dropdown menu

---
+++ parkguidance-web/src/main/java/com/neo/parkguidance/web/infra/entity/converter/EntityParkingGarageConverter.java
@@ -0,0 +1,36 @@
+package com.neo.parkguidance.web.infra.entity.converter;
+
+import com.neo.parkguidance.core.entity.ParkingGarage;
+import com.neo.parkguidance.web.infra.entity.ParkingGarageEntityService;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+public class EntityParkingGarageConverter implements Converter {
+
+    public static final String BEAN_NAME = "entityParkingConverter";
+
+    @Inject
+    ParkingGarageEntityService parkingGarageEntityService;
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        ParkingGarage parkingGarage = parkingGarageEntityService.find(Long.valueOf(value));
+        if(parkingGarage == null) {
+            return new ParkingGarage();
+        }
+        return parkingGarage;
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        if(!(value instanceof ParkingGarage)) {
+            return "";
+        }
+        return String.valueOf(((ParkingGarage) value).getId());
+    }
+}
