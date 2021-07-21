001dfedace6e0b36b3d488ff71b905379782cfd5
Sandro Tartoni <sandro.tartoni@swislog.com>
2021-07-21 17:43:09 +0200

    added javadoc

--- parkguidance-components/parkguidance-core/src/main/java/com/neo/parkguidance/core/impl/StringUtils.java
+++ parkguidance-components/parkguidance-core/src/main/java/com/neo/parkguidance/core/impl/StringUtils.java
@@ -1,9 +1,18 @@
 package com.neo.parkguidance.core.impl;
 
+/**
+ * Utilities for {@link String}
+ */
 public class StringUtils {
 
     private StringUtils() {}
 
+    /**
+     * Checks if string is null or empty
+     *
+     * @param s string to check
+     * @return true if it's null or empty
+     */
     public static boolean isEmpty(String s) {
         return s == null || s.trim().isEmpty();
     }
