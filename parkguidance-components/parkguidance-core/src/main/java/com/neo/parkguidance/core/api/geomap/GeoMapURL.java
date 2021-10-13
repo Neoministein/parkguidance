08949ef034dd7e14ad3419829bcff7b2e7ef84d3
Sandro Tartoni <sandro.tartoni@swislog.com>
2021-10-13 19:08:28 +0200

    added Javadoc

--- parkguidance-components/parkguidance-core/src/main/java/com/neo/parkguidance/core/api/geomap/GeoMapURL.java
+++ parkguidance-components/parkguidance-core/src/main/java/com/neo/parkguidance/core/api/geomap/GeoMapURL.java
@@ -2,9 +2,24 @@ package com.neo.parkguidance.core.api.geomap;
 
 import com.neo.parkguidance.core.entity.ParkingGarage;
 
+/**
+ * This interface defines the functionality to generate a URL to a geo services based on the ParkingGarage
+ */
 public interface GeoMapURL {
 
+    /**
+     * Returns an URL which will land the user on the same page as a search does
+     *
+     * @param parkingGarage the desired ParkingGarage
+     * @return the URL to the external services
+     */
     String search(ParkingGarage parkingGarage);
 
+    /**
+     * Returns an URL which will land the user on a directions page to the ParkingGarage
+     *
+     * @param parkingGarage the desired ParkingGarage
+     * @return the URL to the external service
+     */
     String direction(ParkingGarage parkingGarage);
 }
